9cd8261e5da0aa9b3b23329394802933e8042912
CanhToan <CanhToan@CanhToan>
2017-12-06 15:05:32 +0700

    a

--- src/servlet/DangKyDeTai.java
+++ src/servlet/DangKyDeTai.java
@@ -22,57 +22,46 @@ import model.BoMon;
  */
 @WebServlet("/DangKyDeTai")
 public class DangKyDeTai extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public DangKyDeTai() {
-	super();
-	// TODO Auto-generated constructor stub
-    }
-
-    /*
-     * public void init(ServletConfig config) throws ServletException {
-     * super.init(config); try { Class.forName("com.mysql.jdbc.Driver");//
-     * initialize jdbc Driver } catch (ClassNotFoundException e) { throw new
-     * ServletException(e); } }
-     */
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-	    throws ServletException, IOException {
-	List<BoMon> listBomon = new ArrayList<BoMon>();
-	Connection c = null;
-	try {
-	    // String url =
-	    // "jdbc:mysql://localhost/quanlydetainghiencuukhoahoc?user=root&password=15110376";
-	    // c = (Connection) DriverManager.getConnection(url);
-	    c = connect.DBConnect.getConnection();
-	    Statement stmt = (Statement) c.createStatement();
-	    ResultSet rs = stmt.executeQuery("select * from bomon");
-	    while (rs.next()) {
-		BoMon bomon = new BoMon(rs.getInt("MaBoMon"), rs.getString("TenBoMon"), rs.getInt("MaTruongBoMon"));
-		listBomon.add(bomon);
-	    }
-	} catch (SQLException e) {
-	    throw new ServletException(e);
-	} finally {
-	    try {
-		if (c != null) {
-		    c.close();
-		}
-	    } catch (SQLException e) {
-		throw new ServletException(e);
-	    }
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public DangKyDeTai() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
-	request.setAttribute("listBomon", listBomon);
-	request.getRequestDispatcher("/WEB-INF/dangkydetai.jsp").forward(request, response);
-    }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-	    throws ServletException, IOException {
-	// TODO Auto-generated method stub
-	doGet(request, response);
-    }
+	/*
+	 * public void init(ServletConfig config) throws ServletException {
+	 * super.init(config); try { Class.forName("com.mysql.jdbc.Driver");//
+	 * initialize jdbc Driver } catch (ClassNotFoundException e) { throw new
+	 * ServletException(e); } }
+	 */
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		/*
+		 * List<BoMon> listBomon = new ArrayList<BoMon>(); Connection c = null; try { //
+		 * String url = //
+		 * "jdbc:mysql://localhost/quanlydetainghiencuukhoahoc?user=root&password=15110376";
+		 * // c = (Connection) DriverManager.getConnection(url); c =
+		 * connect.DBConnect.getConnection(); Statement stmt = (Statement)
+		 * c.createStatement(); ResultSet rs = stmt.executeQuery("select * from bomon");
+		 * while (rs.next()) { BoMon bomon = new BoMon(rs.getInt("MaBoMon"),
+		 * rs.getString("TenBoMon"), rs.getInt("MaTruongBoMon")); listBomon.add(bomon);
+		 * } } catch (SQLException e) { throw new ServletException(e); } finally { try {
+		 * if (c != null) { c.close(); } } catch (SQLException e) { throw new
+		 * ServletException(e); } }
+		 */
+		//request.setAttribute("listBomon", listBomon);
+		request.getRequestDispatcher("/WEB-INF/dangkydetai.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
 
 }
